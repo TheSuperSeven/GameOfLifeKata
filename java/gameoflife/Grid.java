a7124f0e03e3e6d2dce731051bd8125e25aa8d6f
E.BARBOSA <ebarbosa4@myges.fr>
2021-04-03 16:37:21 +0200

    génération tableau rdm

--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -6,6 +6,7 @@ public class Grid {
     private Cell[][] cells;
     private int sizeGrid;
     private Random rd;
+    int M = 10, N = 10;
 
     public Grid(int sizeGrid) {
         this.rd = new Random();
@@ -19,8 +20,17 @@ public class Grid {
     }
 
     private void generateRandomInitialState() {
-        // TODO
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if((int)Math.random() % 2 == 1){
+                    cells[i][j] = new Cell(true);
+                }else{
+                    cells[i][j] = new Cell(false);
+                }
+            }
+        }
     }
+
 
     public void generateNextState() {
         // TODO
@@ -29,5 +39,58 @@ public class Grid {
     public String toString() {
         // TODO
         return "";
+    }
+
+    static void nextGeneration(int grid[][], int M, int N)
+    {
+        int[][] future = new int[M][N];
+
+        // Loop through every cell
+        for (int l = 1; l < M - 1; l++)
+        {
+            for (int m = 1; m < N - 1; m++)
+            {
+                // finding no Of Neighbours that are alive
+                int aliveNeighbours = 0;
+                for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                        aliveNeighbours += grid[l + i][m + j];
+
+                // The cell needs to be subtracted from
+                // its neighbours as it was counted before
+                aliveNeighbours -= grid[l][m];
+
+                // Implementing the Rules of Life
+
+                // Cell is lonely and dies
+                if ((grid[l][m] == 1) && (aliveNeighbours < 2))
+                    future[l][m] = 0;
+
+                    // Cell dies due to over population
+                else if ((grid[l][m] == 1) && (aliveNeighbours > 3))
+                    future[l][m] = 0;
+
+                    // A new cell is born
+                else if ((grid[l][m] == 0) && (aliveNeighbours == 3))
+                    future[l][m] = 1;
+
+                    // Remains the same
+                else
+                    future[l][m] = grid[l][m];
+            }
+        }
+
+        System.out.println("Next Generation");
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (future[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
